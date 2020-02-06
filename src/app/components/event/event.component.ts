7d8b56d87d4f1df8c0e65bdcfbaaa2f02e6f9374
isidoreduc <isidoreducasse79@gmail.com>
2020-02-06 16:55:22 +0100

    event list with event removal

--- src/app/components/event/event.component.ts
+++ src/app/components/event/event.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-event',
@@ -7,9 +7,14 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class EventComponent implements OnInit {
   @Input() value: any;
-  constructor() { }
+  @Output() deleteEventItem: EventEmitter<any> = new EventEmitter<any>();
+  isLogged = true;
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  handleDeleteClick() {
+    this.deleteEventItem.emit(this.value);
   }
-
 }
