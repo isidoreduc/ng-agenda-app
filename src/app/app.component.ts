90bc5b0b4144ad8f715ffd62e3f4724a2d939411
isidoreduc <isidoreducasse79@gmail.com>
2020-02-06 04:14:56 +0100

    passing data in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  event: any = {
+    time: '08:00',
+    subject: 'Breakfast with Simon',
+    location: 'Lounge Caffe',
+    description: 'Discuss Q3 targets'
+  };
 }
